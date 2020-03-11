ae24b60798a40824a5f8a53beff34d04a8879235
Matheus Augusto Simões <201811000014@IFSMP.PAS.IFSULDEMINAS.EDU.BR>
2020-03-11 11:57:20 -0300

    avanço na matriz

--- src/Estruturas/MatrizADJ.java
+++ src/Estruturas/MatrizADJ.java
@@ -31,8 +31,69 @@ public int user2(){
     return this.user2;
 } 
 }
-private int mat[][] ; //
-private int contUsers;
-private int posUsers[] ;
+private int maxUsers = 0;
+private int mat[][] ; //definição da matriz
+private int contUsers; // contador de usuarios
+private int posUsers[] ;// vetor de busca de usuarios
+
+public MatrizADJ(int contUsers){
+this.mat = new int [contUsers][contUsers];
+this.posUsers = new int [contUsers];
+this.contUsers = contUsers;
+this.maxUsers = maxUsers;
+
+    for ( int i = 0; i < this.contUsers; i ++) {
+        for ( int j = 0; j < this.contUsers; j ++){
+            this.mat[i][j] = 0;
+            this.posUsers[i] = -1;  
+        }
+    }
+}
+
+public void insereRelacao ( int user1, int user2, int tempo) {
+this .mat[user1][user2] = tempo; 
+}
+public boolean existeRelacao ( int user1, int user2) {
+return (this .mat[user1] [user2] > 0);
+}
+
+public boolean listaAdjVazia ( int v) {
+for ( int i =0; i < this.contUsers; i++)
+if (this .mat[v] [ i ] > 0) return false;
+return true;
+}
+
+public Relacao retiraAresta ( int v1, int v2) {
+    if (this.mat[v1][v2] == 0) return null ; // Aresta não existe
+    else {
+    Relacao relacao = new Relacao (v1, v2, this .mat[v1] [v2] ) ;
+    this.mat[v1] [v2] = 0; return relacao;
+    }
+}
+
+
+public void imprime ( ) {
+    System.out. print ( " " );
+    for ( int i = 0; i < this.contUsers; i++)
+    System.out. print ( i + " " );
+    System.out. println ( ) ;
+    for ( int i = 0; i < this.contUsers; i ++) {
+    System.out. print ( i + " " );
+    for ( int j = 0; j < this.contUsers; j++)
+    System.out. print (this .mat[ i ] [ j ] + " " );
+    System.out. println ( ) ;
+    }
+}
+public int numVertices ( ) {
+return this.contUsers;
+}
+
+public void CadastraUsuario(){
+    if(this.maxUsers<50){
+        
+    }
+    //TODO função que alerta que o limite de usuario foi atingido
+}
+        
 
 }
---
+++ src/Estruturas/Usuario.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Estruturas;
+
+/**
+ *
+ * @author 201811000014
+ */
+public class Usuario {
+    
+}
