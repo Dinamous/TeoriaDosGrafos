1c2336b4aa7bdb6bcca5d85366e71514048d21bb
Matheus <matheusaugustosimoes@hotmail.com>
2020-03-26 15:53:54 -0300

    interface pronta

--- src/teoriadosgrafos/TeoriaDosGrafos.java
+++ src/teoriadosgrafos/TeoriaDosGrafos.java
@@ -5,6 +5,14 @@
  */
 package teoriadosgrafos;
 
+import gui.TelaMain;
+import gui.TelaPrincipal;
+import gui.TelaPrincipal;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
 /**
  *
  * @author 201811000014
@@ -15,7 +23,12 @@ public class TeoriaDosGrafos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        TelaMain TP = new TelaMain();
     
+       
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        TP.setLocation(dim.width/2-TP.getSize().width/2, dim.height/2-TP.getSize().height/2);
+       
+        TP.setVisible(true);
+    }
 }
